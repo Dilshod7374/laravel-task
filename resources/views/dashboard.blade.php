9089461c487e133acc275db2218e24292a9f0594
Dilshod7374 <115866737+Dilshod7374@users.noreply.github.com>
2023-04-03 09:50:51 +0500

    added new applications

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,77 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                <div class="p-2 text-gray-900 dark:text-gray-100">
+                    @if (auth()->user()->role->name == 'manager')
+                        <span class="text-xl">Received Applications</span>
+                        <div class='mt-5 p-6 text-gray-900 dark:text-gray-100'>
+                            @foreach ($applications as $application)
+                                <div style="margin-left: 100px"
+                                    class="rounded-xl border p-5 mt-5 shadow-md w-9/12 bg-white">
+                                    <div class="flex w-full items-center justify-between border-b pb-3">
+                                        <div class="flex items-center space-x-3">
+                                            <div
+                                                class="h-8 w-8 rounded-full bg-slate-400 bg-[url('https://i.pravatar.cc/32')]">
+                                            </div>
+                                            <div class="text-lg font-bold text-slate-700">{{$application->user->name}}
+                                            </div>
+                                        </div>
+                                        <div class="flex items-center space-x-8">
+                                            <button
+                                                class="text-xs text-neutral-500 rounded-2xl border bg-neutral-100 px-3 py-1 text-xs ">#
+                                                {{ $application->id }}
+                                            </button>
+                                            <div class="text-xs text-neutral-500">{{$application->created_at}}</div>
+                                        </div>
+                                    </div>
+                                    <div class="mt-4 mb-6">
+                                        <div class="mb-3 text-xl font-bold text-sm text-neutral-600">{{$application->subject}}</div>
+                                        <div class="text-sm text-neutral-600">{{$application->message}}</div>
+                                    </div>
+                                    <div>
+                                        <div class="flex items-center justify-between text-slate-500">
+                                            {{$application->user->email}}
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+
+                        </div>
+                        {{ $applications->links() }}
+                    @else
+                        <div class='flex items-center justify-center min-h-screen '>
+                            <div class='w-full max-w-lg px-10 py-8 mx-auto bg-white rounded-lg shadow-xl'>
+                                <div class='max-w-md mx-auto space-y-6'>
+                                    <form action="{{ route('applications.store') }}" method="POST"
+                                        enctype="multipart/form-data">
+                                        @csrf
+                                        <h2 class="mb-3 text-xl font-bold text-sm text-neutral-600 ">Submit your
+                                            application</h2>
+                                        <hr class="my-6">
+                                        <label class="mb-3 text-xl font-bold text-sm text-neutral-600">Subject</label>
+                                        <input type="text" required name="subject"
+                                            class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none text-neutral-600">
+
+                                        <label class="mb-3 text-xl font-bold text-sm text-neutral-600">Message</label>
+                                        <textarea name="message" required
+                                            class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none text-neutral-600"
+                                            name="" id="" rows="5"></textarea><br>
+                                        <label class="mb-3 text-xl font-bold text-sm text-neutral-600">File</label>
+                                        <input type="file" name="file"
+                                            class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none text-neutral-600">
+                                        <div>
+                                            <input type="submit"
+                                                class="py-3 px-6 my-2 bg-emerald-500 text-white font-medium rounded hover:bg-indigo-500 cursor-pointer ease-in-out duration-300"
+                                                value="Send">
+                                        </div>
+                                    </form>
+
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+
+
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ApplicationController;
+use App\Http\Controllers\MainController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,18 +16,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('/', [MainController::class, 'main'])->name('main');
+    Route::get('/dashboard',[MainController::class,'dashboard'])->name('dashboard');
+
+    Route::resource('applications',ApplicationController::class);
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+
 
 Route::middleware('auth')->group(function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
 
 require __DIR__.'/auth.php';
